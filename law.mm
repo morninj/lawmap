ac900e608daf7fc9acdf07d934d0a3f0fb8037c4
Joe Mornin <joe@mornin.org>
2017-04-06 11:15:04 -0700

    Add first map

---
+++ law.mm
@@ -0,0 +1,15 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1491502172541" ID="ID_613936847" MODIFIED="1491502177248" TEXT="Law">
+<node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1491502182390" POSITION="right" TEXT="Civil Procedure">
+<node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491502202858" TEXT="Federal">
+<node CREATED="1491502190402" ID="ID_492885197" MODIFIED="1491502195446" TEXT="Default">
+<node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491502393414" TEXT="FRCP 55"/>
+<node CREATED="1491502213766" ID="ID_284456760" MODIFIED="1491502259285" TEXT="&quot;Default&quot;: Defendant fails to defend. Case can continue."/>
+<node CREATED="1491502249365" ID="ID_1663185558" MODIFIED="1491502294914" TEXT="&quot;Entry of default&quot;: Plaintiff must request. Cuts of defendant&apos;s right to defend."/>
+<node CREATED="1491502296805" ID="ID_1898145266" MODIFIED="1491502336690" TEXT="&quot;Default judgment&apos;: After entry, plaintiff can apply for judgment."/>
+</node>
+</node>
+</node>
+</node>
+</map>
