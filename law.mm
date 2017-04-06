3363eddced631893a57ad7f282abbd05bad3f992
Joe Mornin <joe@mornin.org>
2017-04-06 14:17:41 -0700

    Fold

--- law.mm
+++ law.mm
@@ -1,8 +1,8 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1491502172541" ID="ID_613936847" MODIFIED="1491506013083" TEXT="Law">
-<node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1491506979354" POSITION="right" TEXT="Civil Procedure">
-<node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491506980473" TEXT="Federal">
+<node CREATED="1491502179253" FOLDED="true" ID="ID_1181182025" MODIFIED="1491513433958" POSITION="right" TEXT="Civil Procedure">
+<node CREATED="1491502200786" FOLDED="true" ID="ID_1322443873" MODIFIED="1491513069331" TEXT="Federal">
 <node CREATED="1491502190402" FOLDED="true" ID="ID_492885197" MODIFIED="1491511126951" TEXT="Default">
 <node CREATED="1491502752780" ID="ID_405450802" MODIFIED="1491502780994" TEXT="Defendant usually has 21 days to answer or file Rule 12 motion. FRCP 12(a)(1)."/>
 <node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491505709936" TEXT="Default rules: FRCP 55."/>
@@ -16,10 +16,10 @@
 <node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491505692291" TEXT="Sample order granting default judgment"/>
 </node>
 </node>
-<node CREATED="1491511128240" ID="ID_1473443067" MODIFIED="1491511130836" TEXT="Consolidation">
+<node CREATED="1491511128240" FOLDED="true" ID="ID_1473443067" MODIFIED="1491513067501" TEXT="Consolidation">
 <node CREATED="1491511134823" ID="ID_893823412" MODIFIED="1491511141844" TEXT="FRCP 42(a)"/>
-<node CREATED="1491511142051" ID="ID_1221784016" MODIFIED="1491511880478" TEXT="Related cases&#x2014;N.D. Cal. Civ. LR 3-12">
-<node CREATED="1491511241120" ID="ID_307501864" MODIFIED="1491511254469" TEXT="Party in lowest-numbered case must file administrative motion to relate.">
+<node CREATED="1491511142051" FOLDED="true" ID="ID_1221784016" MODIFIED="1491513066540" TEXT="Related cases&#x2014;N.D. Cal. Civ. LR 3-12">
+<node CREATED="1491511241120" FOLDED="true" ID="ID_307501864" MODIFIED="1491513065797" TEXT="Party in lowest-numbered case must file administrative motion to relate.">
 <node CREATED="1491512222939" ID="ID_1068061415" LINK="https://ia601501.us.archive.org/22/items/MotionToRelateCases/motion%20to%20relate%20cases.pdf" MODIFIED="1491512229374" TEXT="Sample motion to relate cases"/>
 <node CREATED="1491512231367" ID="ID_972239035" LINK="https://archive.org/download/MotionToRelateCases/related%20case%20order.pdf" MODIFIED="1491512257462" TEXT="Sample order relating cases"/>
 </node>
