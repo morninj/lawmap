9199d7a718ccd5e79a99ce38901d24a9ba9d88b1
Joe Mornin <joe@mornin.org>
2017-04-06 12:13:52 -0700

    Expand default

--- law.mm
+++ law.mm
@@ -4,10 +4,17 @@
 <node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1491502182390" POSITION="right" TEXT="Civil Procedure">
 <node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491502202858" TEXT="Federal">
 <node CREATED="1491502190402" ID="ID_492885197" MODIFIED="1491502195446" TEXT="Default">
-<node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491502393414" TEXT="FRCP 55"/>
+<node CREATED="1491502752780" ID="ID_405450802" MODIFIED="1491502780994" TEXT="Defendant usually has 21 days to answer or file Rule 12 motion. FRCP 12(a)(1)."/>
+<node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491505709936" TEXT="Default rules: FRCP 55."/>
 <node CREATED="1491502213766" ID="ID_284456760" MODIFIED="1491502259285" TEXT="&quot;Default&quot;: Defendant fails to defend. Case can continue."/>
-<node CREATED="1491502249365" ID="ID_1663185558" MODIFIED="1491502294914" TEXT="&quot;Entry of default&quot;: Plaintiff must request. Cuts of defendant&apos;s right to defend."/>
-<node CREATED="1491502296805" ID="ID_1898145266" MODIFIED="1491502336690" TEXT="&quot;Default judgment&apos;: After entry, plaintiff can apply for judgment."/>
+<node CREATED="1491502249365" ID="ID_1663185558" MODIFIED="1491504893253" TEXT="&quot;Entry of default&quot;: Plaintiff must request. Cuts off defendant&apos;s right to defend. Clerk will enter without court action.">
+<node CREATED="1491504870236" ID="ID_496466740" LINK="https://archive.org/download/RequestForEntryOfDefault/request%20for%20entry%20of%20default.pdf" MODIFIED="1491504918524" TEXT="Sample request for entry of default"/>
+<node CREATED="1491505092479" ID="ID_1523070146" LINK="https://archive.org/download/SampleClerksNoticeOfEntryOfDefault/sample%20clerks%20notice%20of%20entry%20of%20default.pdf" MODIFIED="1491505162000" TEXT="Sample clerk&apos;s notice of entry of default"/>
+</node>
+<node CREATED="1491502296805" ID="ID_1898145266" MODIFIED="1491505816613" TEXT="&quot;Default judgment&apos;: After entry, plaintiff can apply for judgment. Clerk can sometimes enter default judgment. Usually a &quot;hearing&quot; is required (but the motion can be taken under submission; an in-person hearing is not mandatory).">
+<node CREATED="1491505500222" ID="ID_1360772065" LINK="https://archive.org/download/SampleMotionForDefaultJudgment/sample%20motion%20for%20default%20judgment.pdf" MODIFIED="1491505588603" TEXT="Sample motion for default judgment"/>
+<node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491505692291" TEXT="Sample order granting default judgment"/>
+</node>
 </node>
 </node>
 </node>
