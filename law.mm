4b374df8afe914dcd4c2f093792060a13691e471
Joe Mornin <joe@mornin.org>
2017-04-06 14:10:25 -0700

    Add consolidation

--- law.mm
+++ law.mm
@@ -3,7 +3,7 @@
 <node CREATED="1491502172541" ID="ID_613936847" MODIFIED="1491506013083" TEXT="Law">
 <node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1491506979354" POSITION="right" TEXT="Civil Procedure">
 <node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491506980473" TEXT="Federal">
-<node CREATED="1491502190402" ID="ID_492885197" MODIFIED="1491506981512" TEXT="Default">
+<node CREATED="1491502190402" FOLDED="true" ID="ID_492885197" MODIFIED="1491511126951" TEXT="Default">
 <node CREATED="1491502752780" ID="ID_405450802" MODIFIED="1491502780994" TEXT="Defendant usually has 21 days to answer or file Rule 12 motion. FRCP 12(a)(1)."/>
 <node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491505709936" TEXT="Default rules: FRCP 55."/>
 <node CREATED="1491502213766" ID="ID_284456760" MODIFIED="1491502259285" TEXT="&quot;Default&quot;: Defendant fails to defend. Case can continue."/>
@@ -14,6 +14,16 @@
 <node CREATED="1491502296805" ID="ID_1898145266" MODIFIED="1491506992665" TEXT="&quot;Default judgment&quot;: After entry, plaintiff can apply for judgment. Clerk can sometimes enter default judgment. Usually a &quot;hearing&quot; is required (but the motion can be taken under submission; an in-person hearing is not mandatory).">
 <node CREATED="1491505500222" ID="ID_1360772065" LINK="https://archive.org/download/SampleMotionForDefaultJudgment/sample%20motion%20for%20default%20judgment.pdf" MODIFIED="1491505588603" TEXT="Sample motion for default judgment"/>
 <node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491505692291" TEXT="Sample order granting default judgment"/>
+</node>
+</node>
+<node CREATED="1491511128240" ID="ID_1473443067" MODIFIED="1491511130836" TEXT="Consolidation">
+<node CREATED="1491511134823" ID="ID_893823412" MODIFIED="1491511141844" TEXT="FRCP 42(a)"/>
+<node CREATED="1491511142051" ID="ID_1221784016" MODIFIED="1491511880478" TEXT="Related cases&#x2014;N.D. Cal. Civ. LR 3-12">
+<node CREATED="1491511241120" ID="ID_307501864" MODIFIED="1491511254469" TEXT="Party in lowest-numbered case must file administrative motion to relate.">
+<node CREATED="1491512222939" ID="ID_1068061415" LINK="https://ia601501.us.archive.org/22/items/MotionToRelateCases/motion%20to%20relate%20cases.pdf" MODIFIED="1491512229374" TEXT="Sample motion to relate cases"/>
+<node CREATED="1491512231367" ID="ID_972239035" LINK="https://archive.org/download/MotionToRelateCases/related%20case%20order.pdf" MODIFIED="1491512257462" TEXT="Sample order relating cases"/>
+</node>
+<node CREATED="1491511904279" ID="ID_241403870" MODIFIED="1491511930280" TEXT="This is a local rule. The idea of &quot;related cases&quot; doesn&apos;t appear in the federal rules."/>
 </node>
 </node>
 </node>
