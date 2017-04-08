421e4516a836d6f7db14847510582f5f6a1cf82b
Joe Mornin <joe@mornin.org>
2017-04-07 18:18:28 -0700

    Expand judgment

--- law.mm
+++ law.mm
@@ -1,9 +1,9 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1491502172541" ID="ID_613936847" MODIFIED="1491506013083" TEXT="Law">
-<node CREATED="1491502179253" FOLDED="true" ID="ID_1181182025" MODIFIED="1491513433958" POSITION="right" TEXT="Civil Procedure">
-<node CREATED="1491502200786" FOLDED="true" ID="ID_1322443873" MODIFIED="1491513069331" TEXT="Federal">
-<node CREATED="1491502190402" FOLDED="true" ID="ID_492885197" MODIFIED="1491511126951" TEXT="Default">
+<node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1491581053459" POSITION="right" TEXT="Civil Procedure">
+<node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491581054406" TEXT="Federal">
+<node CREATED="1491502190402" ID="ID_492885197" MODIFIED="1491613366031" TEXT="Default">
 <node CREATED="1491502752780" ID="ID_405450802" MODIFIED="1491502780994" TEXT="Defendant usually has 21 days to answer or file Rule 12 motion. FRCP 12(a)(1)."/>
 <node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491505709936" TEXT="Default rules: FRCP 55."/>
 <node CREATED="1491502213766" ID="ID_284456760" MODIFIED="1491502259285" TEXT="&quot;Default&quot;: Defendant fails to defend. Case can continue."/>
@@ -11,9 +11,9 @@
 <node CREATED="1491504870236" ID="ID_496466740" LINK="https://archive.org/download/RequestForEntryOfDefault/request%20for%20entry%20of%20default.pdf" MODIFIED="1491504918524" TEXT="Sample request for entry of default"/>
 <node CREATED="1491505092479" ID="ID_1523070146" LINK="https://archive.org/download/SampleClerksNoticeOfEntryOfDefault/sample%20clerks%20notice%20of%20entry%20of%20default.pdf" MODIFIED="1491505162000" TEXT="Sample clerk&apos;s notice of entry of default"/>
 </node>
-<node CREATED="1491502296805" ID="ID_1898145266" MODIFIED="1491506992665" TEXT="&quot;Default judgment&quot;: After entry, plaintiff can apply for judgment. Clerk can sometimes enter default judgment. Usually a &quot;hearing&quot; is required (but the motion can be taken under submission; an in-person hearing is not mandatory).">
+<node CREATED="1491502296805" ID="ID_1898145266" MODIFIED="1491613367745" TEXT="&quot;Default judgment&quot;: After entry, plaintiff can apply for judgment. Clerk can sometimes enter default judgment. Usually a &quot;hearing&quot; is required (but the motion can be taken under submission; an in-person hearing is not mandatory).">
 <node CREATED="1491505500222" ID="ID_1360772065" LINK="https://archive.org/download/SampleMotionForDefaultJudgment/sample%20motion%20for%20default%20judgment.pdf" MODIFIED="1491505588603" TEXT="Sample motion for default judgment"/>
-<node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491505692291" TEXT="Sample order granting default judgment"/>
+<node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491613505610" TEXT="Sample default judgment"/>
 </node>
 </node>
 <node CREATED="1491511128240" FOLDED="true" ID="ID_1473443067" MODIFIED="1491513067501" TEXT="Consolidation">
@@ -24,6 +24,36 @@
 <node CREATED="1491512231367" ID="ID_972239035" LINK="https://archive.org/download/MotionToRelateCases/related%20case%20order.pdf" MODIFIED="1491512257462" TEXT="Sample order relating cases"/>
 </node>
 <node CREATED="1491511904279" ID="ID_241403870" MODIFIED="1491511930280" TEXT="This is a local rule. The idea of &quot;related cases&quot; doesn&apos;t appear in the federal rules."/>
+</node>
+</node>
+<node CREATED="1491581055948" FOLDED="true" ID="ID_1543603689" MODIFIED="1491613365266" TEXT="Settlement">
+<node CREATED="1491581347646" ID="ID_1130430105" MODIFIED="1491581355413" TEXT="Settling after complaint has been filed">
+<node CREATED="1491581357310" FOLDED="true" ID="ID_1329938103" MODIFIED="1491583738739" TEXT="Parties must notify court promptly.">
+<node CREATED="1491583719815" ID="ID_678609437" LINK="https://archive.org/download/OrderGrantingVoluntaryDismissal/notice%20of%20settlement.pdf" MODIFIED="1491583731415" TEXT="Sample notice of settlement"/>
+</node>
+<node CREATED="1491583739363" FOLDED="true" ID="ID_615416423" MODIFIED="1491598548182" TEXT="Court might set dates for submission of settlement papers and dismissal forms, or set a status conference.">
+<node CREATED="1491583764409" ID="ID_753433753" LINK="https://archive.org/download/OrderGrantingVoluntaryDismissal/order%20re%20notice%20of%20tentative%20settlement.pdf" MODIFIED="1491583787250" TEXT="Sample order re notice of tentative settlement"/>
+<node CREATED="1491583934020" ID="ID_1106692093" LINK="https://ia601501.us.archive.org/6/items/OrderGrantingVoluntaryDismissal/osc%20re%20settlement.pdf" MODIFIED="1491583944104" TEXT="Sample OSC re settlement"/>
+<node CREATED="1491583876244" ID="ID_998393520" MODIFIED="1491583881627" TEXT="Sample docket entry: &quot;ORDER RE: NOTICE OF TENTATIVE SETTLEMENT. All pretrial deadlines and hearing dates set in each case, including Case Management Conferences, are VACATED and any pending motions are TERMINATED. The court schedules a Status Conference for 10:00 a.m. on 6/29/2017. On or before 6/22/2017, the parties shall file a Joint Status Conference Statement which provides, inter alia, an update on the settlement of these actions. Signed by Judge Edward J. Davila on 4/7/2017. (ejdlc1S, COURT STAFF) (Filed on 4/7/2017) (Entered: 04/07/2017)&quot;"/>
+</node>
+<node CREATED="1491583956959" FOLDED="true" ID="ID_1816058008" MODIFIED="1491598551326" TEXT="Parties usually file stipulation of dismissal.">
+<node CREATED="1491584106362" ID="ID_404122110" LINK="https://archive.org/download/OrderGrantingVoluntaryDismissal/stipulation%20of%20dismissal.pdf" MODIFIED="1491584118860" TEXT="Sample stipulation of dismissal"/>
+<node CREATED="1491583978775" ID="ID_1066693131" LINK="https://archive.org/download/OrderGrantingVoluntaryDismissal/notice%20of%20voluntary%20dismissal.pdf" MODIFIED="1491584001554" TEXT="Sample notice of voluntary dismissal by plaintiff"/>
+</node>
+<node CREATED="1491584122313" FOLDED="true" ID="ID_495447010" MODIFIED="1491598552234" TEXT="Court closes case.">
+<node CREATED="1491584033079" ID="ID_1428727450" LINK="https://archive.org/download/OrderGrantingVoluntaryDismissal/order%20granting%20voluntary%20dismissal.pdf" MODIFIED="1491584043835" TEXT="Sample order granting voluntary dismissal"/>
+<node CREATED="1491583358136" ID="ID_1778508251" MODIFIED="1491583367339" TEXT="Sample docket entry closing case: &quot;***Civil Case Terminated pursuant to Notice of Dismissal 52 and Stipulation of Dismissal 54 . (lmh, COURT STAFF) (Filed on 6/8/2016) (Entered: 06/08/2016)&quot;"/>
+</node>
+<node CREATED="1491581063690" ID="ID_1647662973" MODIFIED="1491581109024" TEXT="Judge Davila standing order: parties must file Notice of Settlement saying when they expect the case to be resolved (e.g., through stipulated dismissal)."/>
+</node>
+</node>
+<node CREATED="1491613327722" ID="ID_1761576248" MODIFIED="1491613425544" TEXT="Entry of judgment">
+<node CREATED="1491613332999" ID="ID_619156838" MODIFIED="1491613340575" TEXT="FRCP 58"/>
+<node CREATED="1491613342983" ID="ID_1275492418" MODIFIED="1491613433302" TEXT="Must be a separate document. FRCP 58(a)."/>
+<node CREATED="1491613434385" ID="ID_924105369" MODIFIED="1491614295555" TEXT="Should contain language like: &quot;IT IS HEREBY ORDERED, ADJUDGED AND DECREED THAT Judgment be entered in favor of Plaintiffs and against Defendant as follows:&quot;--and maybe also: &quot;The Clerk shall close this file.&quot;">
+<node CREATED="1491613534102" ID="ID_1320681975" LINK="https://archive.org/download/judgment_201704/judgment.pdf" MODIFIED="1491614273319" TEXT="Sample judgment"/>
+<node CREATED="1491505516335" ID="ID_1072661974" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491613505610" TEXT="Sample default judgment"/>
+<node CREATED="1491613633476" ID="ID_745677008" MODIFIED="1491613674967" TEXT="Sample docket entry: &quot;JUDGMENT. IT IS HEREBY ORDERED, ADJUDGED, AND DECREED that judgment is entered in favor of Plaintiff. The Clerk shall close this file. Signed by Judge...&quot;"/>
 </node>
 </node>
 </node>
