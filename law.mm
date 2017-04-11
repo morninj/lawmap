a9b135011b891e8968fead8b698a01b70a6ff9a1
Joe Mornin <joe@mornin.org>
2017-04-11 13:18:20 -0700

    Add notice of appeal

--- law.mm
+++ law.mm
@@ -2,8 +2,25 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1491502172541" ID="ID_613936847" MODIFIED="1491506013083" TEXT="Law">
 <node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1491581053459" POSITION="right" TEXT="Civil Procedure">
-<node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491581054406" TEXT="Federal">
-<node CREATED="1491502190402" ID="ID_492885197" MODIFIED="1491613366031" TEXT="Default">
+<node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1491941482791" TEXT="Federal">
+<node CREATED="1491939234354" ID="ID_1904091422" MODIFIED="1491941489272" TEXT="District court">
+<node CREATED="1491849771129" FOLDED="true" ID="ID_368835578" MODIFIED="1491939214321" TEXT="Attacking the pleadings">
+<node CREATED="1491849802834" FOLDED="true" ID="ID_114183808" MODIFIED="1491850024670" TEXT="Rule 12 generally">
+<node CREATED="1491849810136" FOLDED="true" ID="ID_84734192" MODIFIED="1491850023940" TEXT="Two types of motions">
+<node CREATED="1491849814848" ID="ID_844253789" MODIFIED="1491849852314" TEXT="Challenging the adequacy of pleadings (failure to state a claim; for more definite statement; to strike)"/>
+<node CREATED="1491849852961" ID="ID_934940286" MODIFIED="1491849917139" TEXT="Challenging jurisdiction and venue (lack of jurisdiction; improper venue; improper service))"/>
+</node>
+</node>
+<node CREATED="1491849926402" FOLDED="true" ID="ID_818532083" MODIFIED="1491939213048" TEXT="Motion for more definite statement (12(e))">
+<node CREATED="1491850170061" FOLDED="true" ID="ID_1984140100" MODIFIED="1491939212198" TEXT="Can defendant ascertain the nature of the asserted claim? Can defendant reasonably prepare a response?">
+<node CREATED="1491850089085" ID="ID_700322295" MODIFIED="1491850096853" TEXT="&quot;A party may move for a more definite statement of a pleading to which a responsive pleading is allowed but which is so vague or ambiguous that the party cannot reasonably prepare a response.&quot; FRCP 12(e)."/>
+</node>
+<node CREATED="1491850243689" ID="ID_1809990992" MODIFIED="1491850255344" TEXT="Disfavored and rarely granted."/>
+<node CREATED="1491850307648" ID="ID_1665301538" MODIFIED="1491850316068" TEXT="Must be made before filing of responsive pleading."/>
+<node CREATED="1491850361083" ID="ID_1387116043" MODIFIED="1491850381336" TEXT="Court can require pleadings to be amended, or that the information sought be provided through declarations or other means."/>
+</node>
+</node>
+<node CREATED="1491502190402" FOLDED="true" ID="ID_492885197" MODIFIED="1491844427452" TEXT="Default">
 <node CREATED="1491502752780" ID="ID_405450802" MODIFIED="1491502780994" TEXT="Defendant usually has 21 days to answer or file Rule 12 motion. FRCP 12(a)(1)."/>
 <node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491505709936" TEXT="Default rules: FRCP 55."/>
 <node CREATED="1491502213766" ID="ID_284456760" MODIFIED="1491502259285" TEXT="&quot;Default&quot;: Defendant fails to defend. Case can continue."/>
@@ -16,10 +33,10 @@
 <node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491613505610" TEXT="Sample default judgment"/>
 </node>
 </node>
-<node CREATED="1491511128240" FOLDED="true" ID="ID_1473443067" MODIFIED="1491513067501" TEXT="Consolidation">
+<node CREATED="1491511128240" FOLDED="true" ID="ID_1473443067" MODIFIED="1491849768084" TEXT="Consolidation">
 <node CREATED="1491511134823" ID="ID_893823412" MODIFIED="1491511141844" TEXT="FRCP 42(a)"/>
-<node CREATED="1491511142051" FOLDED="true" ID="ID_1221784016" MODIFIED="1491513066540" TEXT="Related cases&#x2014;N.D. Cal. Civ. LR 3-12">
-<node CREATED="1491511241120" FOLDED="true" ID="ID_307501864" MODIFIED="1491513065797" TEXT="Party in lowest-numbered case must file administrative motion to relate.">
+<node CREATED="1491511142051" ID="ID_1221784016" MODIFIED="1491844432981" TEXT="Related cases&#x2014;N.D. Cal. Civ. LR 3-12">
+<node CREATED="1491511241120" ID="ID_307501864" MODIFIED="1491844434949" TEXT="Party in lowest-numbered case must file administrative motion to relate.">
 <node CREATED="1491512222939" ID="ID_1068061415" LINK="https://ia601501.us.archive.org/22/items/MotionToRelateCases/motion%20to%20relate%20cases.pdf" MODIFIED="1491512229374" TEXT="Sample motion to relate cases"/>
 <node CREATED="1491512231367" ID="ID_972239035" LINK="https://archive.org/download/MotionToRelateCases/related%20case%20order.pdf" MODIFIED="1491512257462" TEXT="Sample order relating cases"/>
 </node>
@@ -47,13 +64,22 @@
 <node CREATED="1491581063690" ID="ID_1647662973" MODIFIED="1491581109024" TEXT="Judge Davila standing order: parties must file Notice of Settlement saying when they expect the case to be resolved (e.g., through stipulated dismissal)."/>
 </node>
 </node>
-<node CREATED="1491613327722" ID="ID_1761576248" MODIFIED="1491613425544" TEXT="Entry of judgment">
+<node CREATED="1491613327722" FOLDED="true" ID="ID_1761576248" MODIFIED="1491941675704" TEXT="Entry of judgment">
 <node CREATED="1491613332999" ID="ID_619156838" MODIFIED="1491613340575" TEXT="FRCP 58"/>
 <node CREATED="1491613342983" ID="ID_1275492418" MODIFIED="1491613433302" TEXT="Must be a separate document. FRCP 58(a)."/>
-<node CREATED="1491613434385" ID="ID_924105369" MODIFIED="1491614295555" TEXT="Should contain language like: &quot;IT IS HEREBY ORDERED, ADJUDGED AND DECREED THAT Judgment be entered in favor of Plaintiffs and against Defendant as follows:&quot;--and maybe also: &quot;The Clerk shall close this file.&quot;">
+<node CREATED="1491613434385" FOLDED="true" ID="ID_924105369" MODIFIED="1491941674846" TEXT="Should contain language like: &quot;IT IS HEREBY ORDERED, ADJUDGED AND DECREED THAT Judgment be entered in favor of Plaintiffs and against Defendant as follows:&quot;--and maybe also: &quot;The Clerk shall close this file.&quot;">
 <node CREATED="1491613534102" ID="ID_1320681975" LINK="https://archive.org/download/judgment_201704/judgment.pdf" MODIFIED="1491614273319" TEXT="Sample judgment"/>
 <node CREATED="1491505516335" ID="ID_1072661974" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491613505610" TEXT="Sample default judgment"/>
 <node CREATED="1491613633476" ID="ID_745677008" MODIFIED="1491613674967" TEXT="Sample docket entry: &quot;JUDGMENT. IT IS HEREBY ORDERED, ADJUDGED, AND DECREED that judgment is entered in favor of Plaintiff. The Clerk shall close this file. Signed by Judge...&quot;"/>
+</node>
+</node>
+</node>
+<node CREATED="1491939239842" ID="ID_1841161810" MODIFIED="1491941015862" TEXT="Appellate court">
+<node CREATED="1491941494797" ID="ID_76725552" MODIFIED="1491941496418" TEXT="Notice">
+<node CREATED="1491939975117" ID="ID_597819705" MODIFIED="1491941528490" TEXT="Must usually be filed within 30 days of entry of judgment or order. FRAP 4."/>
+<node CREATED="1491941575742" ID="ID_943225511" MODIFIED="1491941653115" TEXT="30-day period begins when judgment is entered. Judgment must be in a separate document, FRCP 58. But a party can still appeal if there is not a separate document."/>
+<node CREATED="1491940074535" ID="ID_649237638" MODIFIED="1491941536762" TEXT="Must be filed in the district court from which the appeal is taken. FRAP 3."/>
+<node CREATED="1491941842178" ID="ID_1684031550" LINK="https://archive.org/download/NoticeOfAppeal/notice%20of%20appeal.pdf" MODIFIED="1491941853856" TEXT="Sample notice of appeal"/>
 </node>
 </node>
 </node>
