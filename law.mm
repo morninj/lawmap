c133e39529e21a6a089a21fd7e2e89934b483a2f
Joe Mornin <joe@mornin.org>
2017-04-17 11:01:51 -0700

    Add service of process

--- law.mm
+++ law.mm
@@ -1,10 +1,22 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1491502172541" ID="ID_613936847" MODIFIED="1491506013083" TEXT="Law">
-<node CREATED="1491502179253" FOLDED="true" ID="ID_1181182025" MODIFIED="1492108188709" POSITION="right" TEXT="Civil Procedure">
-<node CREATED="1491502200786" FOLDED="true" ID="ID_1322443873" MODIFIED="1492108187649" TEXT="Federal">
-<node CREATED="1491939234354" FOLDED="true" ID="ID_1904091422" MODIFIED="1492108186665" TEXT="District court">
-<node CREATED="1491849771129" FOLDED="true" ID="ID_368835578" MODIFIED="1491939214321" TEXT="Attacking the pleadings">
+<node CREATED="1491502179253" ID="ID_1181182025" MODIFIED="1492115206531" POSITION="right" TEXT="Civil Procedure">
+<node CREATED="1491502200786" ID="ID_1322443873" MODIFIED="1492115207337" TEXT="Federal">
+<node CREATED="1491939234354" ID="ID_1904091422" MODIFIED="1492450894808" TEXT="District court">
+<node CREATED="1492450897323" ID="ID_1604400774" MODIFIED="1492450900339" TEXT="Service of process">
+<node CREATED="1492450963875" ID="ID_1879976379" MODIFIED="1492450965951" TEXT="FRCP 4"/>
+<node CREATED="1492450905400" ID="ID_979736451" MODIFIED="1492451923202" TEXT="Plaintiff must serve defendants with:">
+<node CREATED="1492450943049" ID="ID_119425638" MODIFIED="1492450946663" TEXT="Copy of the complaint"/>
+<node CREATED="1492450946977" ID="ID_748411678" MODIFIED="1492450950142" TEXT="Summons"/>
+</node>
+<node CREATED="1492450966897" ID="ID_330536159" MODIFIED="1492450971433" TEXT="Must occur within 90 days"/>
+<node CREATED="1492451866657" FOLDED="true" ID="ID_1037221243" MODIFIED="1492452024941" TEXT="N.D. Cal. LR 4-2--must also serve:">
+<node CREATED="1492451899130" ID="ID_1535306808" MODIFIED="1492451920221" TEXT="(a)  A copy of the Order Setting Initial Case Management Conference and ADR deadlines issued pursuant to Civil L.R. 16-2(a), (b) or (c);&#xa;&#xa;(b)  Any pertinent Standing Orders of the assigned Judge;&#xa;&#xa;(c)  A copy of the assigned judge&#x2019;s order and instructions for the preparation of a Case Management Statement or, if none, the Court&#x2019;s form found at cand.uscourts.gov, pursuant to Civil L.R. 16-10; and&#xa;&#xa;(d)  Except in cases assigned at the time of filing to a Magistrate Judge, a copy of the form allowing a party to consent to assignment of the case to a Magistrate Judge"/>
+</node>
+<node CREATED="1492452025478" ID="ID_456365693" LINK="http://www.cand.uscourts.gov/filelibrary/1243/Atty_Case_Opening_Guide_2014.pdf" MODIFIED="1492452055640" TEXT="See Guide to E-Filing New Civil Cases"/>
+</node>
+<node CREATED="1491849771129" FOLDED="true" ID="ID_368835578" MODIFIED="1492450890950" TEXT="Attacking the pleadings">
 <node CREATED="1491849802834" FOLDED="true" ID="ID_114183808" MODIFIED="1491850024670" TEXT="Rule 12 generally">
 <node CREATED="1491849810136" FOLDED="true" ID="ID_84734192" MODIFIED="1491850023940" TEXT="Two types of motions">
 <node CREATED="1491849814848" ID="ID_844253789" MODIFIED="1491849852314" TEXT="Challenging the adequacy of pleadings (failure to state a claim; for more definite statement; to strike)"/>
@@ -19,12 +31,17 @@
 <node CREATED="1491850307648" ID="ID_1665301538" MODIFIED="1491850316068" TEXT="Must be made before filing of responsive pleading."/>
 <node CREATED="1491850361083" ID="ID_1387116043" MODIFIED="1491850381336" TEXT="Court can require pleadings to be amended, or that the information sought be provided through declarations or other means."/>
 </node>
+<node CREATED="1492108002240" ID="ID_93949986" MODIFIED="1492116747093" TEXT="Motion for reconsideration of an order granting dismissal">
+<node CREATED="1492116829655" ID="ID_139934219" MODIFIED="1492116832732" TEXT="FRCP 59(e)"/>
+<node CREATED="1492116859433" ID="ID_1947067154" MODIFIED="1492116927887" TEXT="In N.D. Cal., must seek leave of court first. LR 7-9."/>
+<node CREATED="1492116755767" ID="ID_185801090" MODIFIED="1492116792448" TEXT="&quot;Rule 59(e) of the Federal Rules of Civil Procedure is the proper avenue for filing a motion for reconsideration of a motion to dismiss without leave to amend. See Mir v. Fosburg, 646 F.2d 342, 344 (9th Cir.1980). &#x201c;Under Rule 59(e), a motion for reconsideration should not be granted, absent highly unusual circumstances, unless the district court is presented with newly discovered evidence, committed clear error, or if there is an intervening change in the controlling law.&#x201d; 389 Orange Street Partners v. Arnold, 179 F.3d 656, 665 (9th Cir.1999).&quot;&#xa;&#xa;Ferris v. City of San Jose, No. 11-CV-01752-LHK, 2013 WL 1120805, at *2 (N.D. Cal. Mar. 16, 2013)"/>
 </node>
-<node CREATED="1491502190402" FOLDED="true" ID="ID_492885197" MODIFIED="1491844427452" TEXT="Default">
+</node>
+<node CREATED="1491502190402" FOLDED="true" ID="ID_492885197" MODIFIED="1492451860202" TEXT="Default">
 <node CREATED="1491502752780" ID="ID_405450802" MODIFIED="1491502780994" TEXT="Defendant usually has 21 days to answer or file Rule 12 motion. FRCP 12(a)(1)."/>
 <node CREATED="1491502242499" ID="ID_1404554514" MODIFIED="1491505709936" TEXT="Default rules: FRCP 55."/>
 <node CREATED="1491502213766" ID="ID_284456760" MODIFIED="1491502259285" TEXT="&quot;Default&quot;: Defendant fails to defend. Case can continue."/>
-<node CREATED="1491502249365" FOLDED="true" ID="ID_1663185558" MODIFIED="1491506005351" TEXT="&quot;Entry of default&quot;: Plaintiff must request. Cuts off defendant&apos;s right to defend. Clerk will enter without court action.">
+<node CREATED="1491502249365" ID="ID_1663185558" MODIFIED="1492451856626" TEXT="&quot;Entry of default&quot;: Plaintiff must request. Cuts off defendant&apos;s right to defend. Clerk will enter without court action.">
 <node CREATED="1491504870236" ID="ID_496466740" LINK="https://archive.org/download/RequestForEntryOfDefault/request%20for%20entry%20of%20default.pdf" MODIFIED="1491504918524" TEXT="Sample request for entry of default"/>
 <node CREATED="1491505092479" ID="ID_1523070146" LINK="https://archive.org/download/SampleClerksNoticeOfEntryOfDefault/sample%20clerks%20notice%20of%20entry%20of%20default.pdf" MODIFIED="1491505162000" TEXT="Sample clerk&apos;s notice of entry of default"/>
 </node>
@@ -33,13 +50,7 @@
 <node CREATED="1491505516335" ID="ID_249835001" LINK="https://archive.org/download/DefaultJudgmentOrder/default%20judgment%20order.pdf" MODIFIED="1491613505610" TEXT="Sample default judgment"/>
 </node>
 </node>
-<node CREATED="1492107988855" FOLDED="true" ID="ID_310351738" MODIFIED="1492108170860" TEXT="Motion practice">
-<node CREATED="1492108002240" FOLDED="true" ID="ID_93949986" MODIFIED="1492108170033" TEXT="Motion for reconsideration (before judgment)">
-<node CREATED="1492108066785" ID="ID_490725379" MODIFIED="1492108111725" TEXT="Usually only appropriate when there is new evidence, clear error, or a change in law."/>
-<node CREATED="1492108149281" ID="ID_1007275198" MODIFIED="1492108163823" TEXT="Usually cannot be used to raise facts or arguments that could have been raised earlier."/>
-<node CREATED="1492108123664" ID="ID_1754937942" MODIFIED="1492108137837" TEXT="Not mentioned in FRCPs, but courts have inherent authority."/>
-</node>
-</node>
+<node CREATED="1492107988855" ID="ID_310351738" MODIFIED="1492116713015" TEXT="Motion practice"/>
 <node CREATED="1491511128240" FOLDED="true" ID="ID_1473443067" MODIFIED="1492107987898" TEXT="Consolidation">
 <node CREATED="1491511134823" ID="ID_893823412" MODIFIED="1491511141844" TEXT="FRCP 42(a)"/>
 <node CREATED="1491511142051" ID="ID_1221784016" MODIFIED="1491844432981" TEXT="Related cases&#x2014;N.D. Cal. Civ. LR 3-12">
@@ -71,7 +82,7 @@
 <node CREATED="1491581063690" ID="ID_1647662973" MODIFIED="1491581109024" TEXT="Judge Davila standing order: parties must file Notice of Settlement saying when they expect the case to be resolved (e.g., through stipulated dismissal)."/>
 </node>
 </node>
-<node CREATED="1491613327722" FOLDED="true" ID="ID_1761576248" MODIFIED="1491941675704" TEXT="Entry of judgment">
+<node CREATED="1491613327722" FOLDED="true" ID="ID_1761576248" MODIFIED="1492116710486" TEXT="Entry of judgment">
 <node CREATED="1491613332999" ID="ID_619156838" MODIFIED="1491613340575" TEXT="FRCP 58"/>
 <node CREATED="1491613342983" ID="ID_1275492418" MODIFIED="1491613433302" TEXT="Must be a separate document. FRCP 58(a)."/>
 <node CREATED="1491613434385" FOLDED="true" ID="ID_924105369" MODIFIED="1491941674846" TEXT="Should contain language like: &quot;IT IS HEREBY ORDERED, ADJUDGED AND DECREED THAT Judgment be entered in favor of Plaintiffs and against Defendant as follows:&quot;--and maybe also: &quot;The Clerk shall close this file.&quot;">
@@ -81,7 +92,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1491939239842" FOLDED="true" ID="ID_1841161810" MODIFIED="1492107981919" TEXT="Appellate court">
+<node CREATED="1491939239842" FOLDED="true" ID="ID_1841161810" MODIFIED="1492450893971" TEXT="Appellate court">
 <node CREATED="1491941494797" ID="ID_76725552" MODIFIED="1491941496418" TEXT="Notice">
 <node CREATED="1491939975117" ID="ID_597819705" MODIFIED="1491941528490" TEXT="Must usually be filed within 30 days of entry of judgment or order. FRAP 4."/>
 <node CREATED="1491941575742" ID="ID_943225511" MODIFIED="1491941653115" TEXT="30-day period begins when judgment is entered. Judgment must be in a separate document, FRCP 58. But a party can still appeal if there is not a separate document."/>
